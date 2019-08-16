1fc0342379cdf03f6e4154175dfe073f5e2a4bb7
crescenzo garofalo <crescenzo.garofalo@gmail.com>
2019-08-17 01:09:12 +0200

    cards

--- src/main/webapp/jsx/home.jsx
+++ src/main/webapp/jsx/home.jsx
@@ -1,8 +1,50 @@
+class Card extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  listaPolizze() {
+    
+    var polizzelist = this.props.polizze.split(';');
+    const res = [];
+
+    if (polizzelist.length > 0) {
+      for (var ii=0; ii<polizzelist.length; ii++) {
+        res.push(<p class="card-text">{polizzelist[ii]}</p>);
+      }
+    } else {
+      res.push(<p class="card-text">Nessuna polizza da mostrare</p>);
+    }
+    return res;
+  }
+
+  render() {
+    return(
+      <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">{this.props.title}</h5>
+        {this.listaPolizze()}
+      </div>
+      <div class="card-footer">
+        <small class="text-muted">{this.props.footer}</small>
+      </div>
+    </div>
+    )
+
+  }
+}
+
 class Home extends React.Component {
     render() {
       return (
-        <div>
-          Home page
+        <div class="card-deck">
+          <Card 
+            title="Polizze Recenti"
+            polizze="12345678;987654321;88888888"
+            footer="Ultimo aggiornamento 12/12/2018">
+
+          </Card>
         </div>
       );
     }
